0ec46863c0476dcb8e48ffd382da54a0043b321c
SerzhKonserzh <Serzh-2001BTR@mail.ru>
2021-11-03 23:01:56 +0300

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -81,12 +81,12 @@ function css(){
                 cascade: true
             })
         )
-        .pipe(
+/*         .pipe(
             webpcss({
                 webpClass: '.webp',
                 noWebpClass: '.no-webp'
             })
-        )
+        ) */
         .pipe(dest(path.build.css))
         .pipe(clean_css())
         .pipe(
@@ -158,9 +158,9 @@ gulp.task("svgSprite",function(){
 })
 
 function fontsStyle(params){
-    let file_content = fs.readFileSync(source_folder + '/sass/fonts.scss');
+    let file_content = fs.readFileSync(source_folder + '/sass/fonts.sass');
     if (file_content == '') {
-        fs.writeFile(source_folder + '/sass/fonts.scss', '', cb);
+        fs.writeFile(source_folder + '/sass/fonts.sass', '', cb);
         return fs.readdir(path.build.fonts, function (err, items) {
             if (items) {
                 let c_fontname;
@@ -168,7 +168,7 @@ function fontsStyle(params){
                     let fontname = items[i].split('.');
                     fontname = fontname[0];
                     if (c_fontname != fontname) {
-                        fs.appendFile(source_folder + '/sass/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                        fs.appendFile(source_folder + '/sass/fonts.sass', '@include font("' + fontname + '", "' + fontname + '", "400", "normal")\r\n', cb);
                     }
                     c_fontname = fontname;
                 }
